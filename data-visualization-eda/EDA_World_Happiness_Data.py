42fc9a803c26ef9f66c0be5da2eb38b9b9471875
Sofia Matias <114782592+sofiammatias@users.noreply.github.com>
2023-10-05 11:24:54 +0100

    deleting files

--- data-visualization-eda/EDA_World_Happiness_Data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-#df = pd.read_excel (r'C:\Users\xufia\OneDrive\Ambiente de Trabalho\DataForFigure2_1WHR2021C2.xls')
-df = pd.read_excel (r'C:\Users\xufia\OneDrive\Ambiente de Trabalho\DataForFigure2_1WHR2021C2.xls')
-print (df.info())
-print (df.iloc[-5:,[0,1,5,6, 18]])
